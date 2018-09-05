ddcb6ebad6e2b48dad701c7b256f20e533a77549
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-09-05 17:40:16 -0400

    exercise8 done

---
+++ exercise8.rb
@@ -0,0 +1,52 @@
+
+def run_ex(person_id)
+  puts "How far did person #{person_id} run (in metres)?"
+  distance = gets.to_f
+  puts "How long (in minutes) did person #{person_id} run take to run #{distance} metres?"
+  time = gets.to_f
+
+speed = distance/(time*60)
+return speed
+
+
+end
+
+# puts "How far did person 1 run (in metres)?"
+# distance1 = gets.to_f
+# puts "How long (in minutes) did person 1 run take to run #{distance1} metres?"
+# mins1 = gets.to_f
+#
+
+speed1 = run_ex(1)
+speed2 = run_ex(2)
+speed3 = run_ex(3)
+
+if speed3 > speed2 && speed3 > speed1
+  puts "Person 3 was the fastest at #{speed3} m/s"
+elsif speed2 > speed3 && speed2 > speed1
+  puts "Person 2 was the fastest at #{speed2} m/s"
+elsif speed1 > speed3 && speed1 > speed2
+  puts "Person 1 was the fastest at #{speed1} m/s"
+elsif speed1 == speed2 && speed2 == speed3
+  puts "Everyone tied at #{speed1} m/s"
+else
+  puts "Well done everyone!"
+end
+
+#
+# puts "How far did person 1 run (in metres)?"
+# distance1 = gets.to_f
+# puts "How long (in minutes) did person 1 run take to run #{distance1} metres?"
+# mins1 = gets.to_f
+#
+#
+# puts "How far did person 2 run (in metres)?"
+# distance2 = gets.to_f
+# puts "How long (in minutes) did person 2 take to run #{distance2} metres?"
+# mins2 = gets.to_f
+#
+#
+# puts "How far did person 3 run (in metres)?"
+# distance3 = gets.to_f
+# puts "How long (in minutes) did person 3 take to run #{distance3} metres?"
+# mins3 = gets.to_f
